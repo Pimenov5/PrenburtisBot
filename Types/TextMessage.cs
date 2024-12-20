d17697e32bd4bc3501db50ffd7fcb8cec3ccfd24
Vasilii <pimenovasya@gmail.com>
2024-12-20 12:49:39 +0300

    Added public property NavigateTo to class TextMessage

--- Types/TextMessage.cs
+++ Types/TextMessage.cs
@@ -3,10 +3,26 @@ using TelegramBotBase.Form;
 
 namespace PrenburtisBot.Types
 {
+	internal readonly struct FormWithArgs(FormBase form, params object[] args)
+	{
+		public readonly FormBase Form = form;
+		public readonly object[] Args = args;
+	}
+
 	internal class TextMessage(string text)
 	{
+		public static Func<FormBase>? GetStartForm;
+
 		public readonly string Text = text;
 		public ButtonForm? Buttons;
 		public ParseMode? ParseMode = null;
+		public FormWithArgs NavigateTo;
+
+		public TextMessage NavigateToStart(params object[] args)
+		{
+			FormBase form = GetStartForm?.Invoke() ?? throw new NullReferenceException();
+			this.NavigateTo = new(form, args);
+			return this;
+		}
 	}
 }
