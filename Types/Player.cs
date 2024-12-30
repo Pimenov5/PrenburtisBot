b0f78aaeed645cb399d8f6992ab946e96ff53a24
Vasilii <pimenovasya@gmail.com>
2024-12-30 12:36:39 +0300

    Added private string property _firstName

--- Types/Player.cs
+++ Types/Player.cs
@@ -2,9 +2,11 @@
 {
 	internal class Player(long userId, int rank, string firstName, double rating)
 	{
+		private string _firstName = firstName;
+
 		public readonly long UserId = userId;
 		public readonly int Rank = rank;
-		public readonly string FirstName = firstName;
+		public string FirstName { get { return _firstName; } set { _firstName = string.IsNullOrEmpty(value) ? throw new ArgumentNullException(nameof(FirstName)) : value; } }
 		public string? Username;
 		public readonly double Rating = rating;
 
