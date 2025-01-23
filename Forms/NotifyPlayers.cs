fb728cdf82ff676422f8bcacce75abd6e6edeced
Vasilii <pimenovasya@gmail.com>
2025-01-23 13:51:25 +0300

    Added class NotifyPlayers

---
+++ Forms/NotifyPlayers.cs
@@ -0,0 +1,21 @@
+﻿using PrenburtisBot.Attributes;
+using PrenburtisBot.Types;
+using System.Text;
+using Telegram.Bot.Types.Enums;
+
+namespace PrenburtisBot.Forms
+{
+	[BotCommand("Уведомить игроков из опроса", BotCommandScopeType.AllChatAdministrators)]
+	internal class NotifyPlayers : RepliedToPollGroupFormBase
+	{
+		protected override TextMessage GetTextMessage(long userId, IReadOnlyCollection<Player> players, params string[] args)
+		{
+			StringBuilder stringBuilder = new();
+			stringBuilder.AppendJoin(' ', args);
+			stringBuilder.AppendLine(Environment.NewLine);
+			stringBuilder.AppendJoin(", ", players);	
+
+			return new(stringBuilder.ToString()) { ParseMode = ParseMode.Markdown }; 
+		}
+	}
+}
