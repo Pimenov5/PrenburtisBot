aded2ea3a68fd78973a38f5bd22c762a2f779b81
Vasilii <pimenovasya@gmail.com>
2025-01-23 13:51:06 +0300

    Added class RepliedToPollGroupFormBase

--- Forms/AddPlayers.cs
+++ Forms/AddPlayers.cs
@@ -2,53 +2,30 @@
 using PrenburtisBot.Attributes;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
-using TelegramBotBase.Base;
-using TelegramBotBase.Form;
 using TL;
-using Telegram.Bot;
 
 namespace PrenburtisBot.Forms
 {
 	[BotCommand("Добавить к площадке игроков из опроса", BotCommandScopeType.AllChatAdministrators)]
-	internal class AddPlayers : BotCommandGroupFormBase
+	internal class AddPlayers : RepliedToPollGroupFormBase
 	{
 		private string? _courtId;
 
-		public async Task<TextMessage> RenderAsync(MessageResult message)
+		protected override TextMessage GetTextMessage(long userId, IReadOnlyCollection<Player> players, params string[] args)
 		{
-			if (message.Message.ReplyToMessage is not Telegram.Bot.Types.Message repliedMessage || repliedMessage.Poll is not Telegram.Bot.Types.Poll poll || poll.IsAnonymous
-				|| poll.AllowsMultipleAnswers || poll.Options.Length < 1 || poll.Options[0].Text != SendPoll.PLAYER_JOINED)
-			{
-				return new($"Команда должна вызываться в ответ на не анонимный опрос с первым вариантом ответа \"{SendPoll.PLAYER_JOINED}\"");
-			}
-
-			if (TelegramClient is null)
-				return new("Невозможно получить список проголосовавших в опросе, т.к. вы ещё не авторизовались");
-
-			IReadOnlyCollection<Player> players;
-			try
-			{
-				players = await TelegramClient.GetPlayersFromPoll(repliedMessage, [SendPoll.PLAYER_JOINED_BYTE]);
-			}
-			catch (Exception e)
-			{
-				return new(e.Message);
-			}
-
-			long userId = message.Message.From?.Id ?? throw new NullReferenceException();
-			_courtId = message.BotCommandParameters.Count >= 1 ? message.BotCommandParameters[0] : null;
+			_courtId = args.Length >= 1 ? args[0] : null;
 			Court court = this.GetCourt(players.Count, userId, out int courtId);
 
-			uint?[] teams = court.AddPlayers(players);
+			uint?[] indexes = court.AddPlayers(players);
 			int count = 0;
-			foreach (uint? index in teams)
+			foreach (uint? index in indexes)
 				count = index is null ? count + 1 : count;
 
 			if (count != 0)
-				await this.Device.Send($"Не удалось добавить {count} из {teams.Length} игроков");
+				throw new InvalidOperationException($"Не удалось добавить {count} из {indexes.Length} игроков");
 
 			string text = CourtPlayers.ToString(court, userId, this.Device.IsGroup);
-			return new TextMessage(text) { ParseMode = ParseMode.Markdown, ReplyToMessageId = repliedMessage.MessageId }.NavigateToStart(Start.SET_QUIET);
+			return new TextMessage(text) { ParseMode = ParseMode.Markdown }.NavigateToStart(Start.SET_QUIET);
 		}
 
 		protected virtual Court GetCourt(int playersCount, long userId, out int courtId)
@@ -79,7 +56,5 @@ namespace PrenburtisBot.Forms
 			courtId = int.Parse(_courtId ?? throw new NullReferenceException());
 			return court;
 		}
-
-		public static WTelegram.Client? TelegramClient = null;
 	}
 }
---
+++ Types/RepliedToPollGroupFormBase.cs
@@ -0,0 +1,36 @@
+﻿using PrenburtisBot.Forms;
+using Telegram.Bot;
+using TelegramBotBase.Base;
+
+namespace PrenburtisBot.Types
+{
+	internal abstract class RepliedToPollGroupFormBase : BotCommandGroupFormBase
+	{
+		public static WTelegram.Client? TelegramClient = null;
+
+		protected abstract TextMessage GetTextMessage(long userId, IReadOnlyCollection<Player> players, params string[] args);
+
+		public async Task<TextMessage> RenderAsync(MessageResult message)
+		{
+			if (message.Message.ReplyToMessage is not Telegram.Bot.Types.Message repliedMessage || repliedMessage.Poll is not Telegram.Bot.Types.Poll poll || poll.IsAnonymous
+				|| poll.AllowsMultipleAnswers || poll.Options.Length < 1 || poll.Options[0].Text != SendPoll.PLAYER_JOINED)
+			{
+				return new($"Команда должна вызываться в ответ на не анонимный опрос с первым вариантом ответа \"{SendPoll.PLAYER_JOINED}\"");
+			}
+
+			if (TelegramClient is null)
+				return new("Невозможно получить список проголосовавших в опросе, т.к. вы ещё не авторизовались");
+
+			IReadOnlyCollection<Player> players = await TelegramClient.GetPlayersFromPoll(repliedMessage, [SendPoll.PLAYER_JOINED_BYTE]);
+
+			long userId = message.Message.From?.Id ?? throw new NullReferenceException();
+			string[] args = message.BotCommandParameters.ToArray();
+			if (args[^1].StartsWith('@') && (await this.Client.TelegramClient.GetMeAsync()).Username is string botUsername && args[^1].Equals('@' + botUsername))
+				Array.Resize(ref args, args.Length - 1);
+
+			TextMessage textMessage = this.GetTextMessage(userId, players, args);
+			textMessage.ReplyToMessageId ??= repliedMessage.MessageId;
+			return textMessage;
+		}
+	}
+}
