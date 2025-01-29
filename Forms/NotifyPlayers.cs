d6d685f46b07580bce13afa13808cd8fdb3d6fa2
Vasilii <pimenovasya@gmail.com>
2025-01-29 19:20:54 +0300

    Fixed protected method GetTextMessage

--- Forms/NotifyPlayers.cs
+++ Forms/NotifyPlayers.cs
@@ -15,7 +15,7 @@ namespace PrenburtisBot.Forms
 			stringBuilder.AppendLine(Environment.NewLine);
 			stringBuilder.AppendJoin(", ", players);	
 
-			return new(stringBuilder.ToString()) { ParseMode = ParseMode.Markdown }; 
+			return new TextMessage(stringBuilder.ToString()) { ParseMode = ParseMode.Markdown }.NavigateToStart(Start.SET_QUIET); 
 		}
 	}
 }
