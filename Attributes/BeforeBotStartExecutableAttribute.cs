2d7cb5f31ddca0e10d515532a04f45de68ae27ad
Vasilii <pimenovasya@gmail.com>
2025-02-26 12:28:03 +0300

    Added attribute BeforeBotStartExecutableAttribute

---
+++ Attributes/BeforeBotStartExecutableAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace PrenburtisBot.Attributes
+{
+	[AttributeUsage(AttributeTargets.Class)]
+	internal class BeforeBotStartExecutableAttribute(string methodName) : Attribute
+	{
+		public readonly string MethodName = methodName;
+		public static string GetPath(string variable)
+		{
+			if (Environment.GetEnvironmentVariable(variable) is not string path)
+				throw new Exception($"Отсутствует значение переменной окружения {variable}");
+			if (!File.Exists(path))
+				throw new Exception($"Не существует файла {path}");
+			return path;
+		}
+	}
+}
