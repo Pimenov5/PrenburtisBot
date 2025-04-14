479d0c5358778a4e48e6440672d53cad28276a16
Vasilii <pimenovasya@gmail.com>
2025-04-14 17:42:42 +0300

    Added public method RenderAsync

--- Forms/Start.cs
+++ Forms/Start.cs
@@ -15,6 +15,41 @@ namespace PrenburtisBot.Forms
 			return this.Device.IsGroup || this.Device.IsChannel || mode == SET_QUIET ? null : "Введите команду или выберите из меню";  
 		}
 
+		public async Task<string?> RenderAsync(string strChatId, string strIsConfirmed, string fileId)
+		{
+			static ArgumentException GetException(string value, string format = "Не удалось выделить {0}") => new(string.Format(format, value));
+
+			ChatId chatId = long.TryParse(strChatId, out long longValue) ? longValue : throw GetException("идентификатор пользователя");
+
+			string text = string.Empty;
+			try
+			{
+				bool isConfirmed = bool.TryParse(strIsConfirmed, out bool boolValue) ? boolValue : throw GetException("разрешение на обновление");
+
+				if (isConfirmed)
+				{
+					fileId = string.IsNullOrEmpty(fileId) ? throw GetException($"идентификатор файла") : fileId;
+					using MemoryStream memoryStream = new();
+					Telegram.Bot.Types.File file = await this.Client.TelegramClient.GetInfoAndDownloadFileAsync(fileId, memoryStream);
+					string? path = Environment.GetEnvironmentVariable("TEAMS_NAMES") ?? throw new NullReferenceException();
+					System.IO.File.WriteAllBytes(path, memoryStream.GetBuffer());
+
+					text = "Файл успешно обновлён";
+				}
+				else
+					text = "Обновление файла было отклонено";
+			}
+			catch (Exception e)
+			{
+				Console.Error.WriteLine(e);
+				text = e.Message;
+			}
+
+			if (chatId.Identifier != this.Device.DeviceId)
+				await this.Client.TelegramClient.SendTextMessageAsync(chatId, text);
+			return text;
+		}
+
 		public override async Task PreLoad(MessageResult message)
 		{
 			if (message.IsFirstHandler && message.BotCommandParameters.Count == 1 && message.BotCommandParameters[0].Split(Commands.PARAMS_DELIMITER) is string[] link
