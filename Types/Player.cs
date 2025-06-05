f5e8f126f4ede79db4155fe0db9c2b2b681d7985
Vasilii <pimenovasya@gmail.com>
2025-06-05 13:34:49 +0300

    Added internal class User

--- Types/Player.cs
+++ Types/Player.cs
@@ -3,16 +3,15 @@
 ﻿namespace PrenburtisBot.Types
 {
 	internal enum Gender { Male, Female };
-	internal class Player(long userId, int rank, string firstName, double rating, bool isActual, Gender gender)
+	internal class Player(long userId, string firstName, double rating, Gender gender)
 	{
 		private string _firstName = firstName;
 
 		public readonly long UserId = userId;
-		public readonly int Rank = rank;
+		public int Rank => (int)Math.Truncate(this.Rating);
 		public string FirstName { get { return _firstName; } set { _firstName = string.IsNullOrEmpty(value) ? throw new ArgumentNullException(nameof(FirstName)) : value; } }
 		public string? Username;
 		public readonly double Rating = rating;
-		public readonly bool IsActual = isActual;
 		public readonly Gender Gender = gender;
 
 		public string Link => $"tg://user?id={this.UserId}".Link(this.FirstName);
---
+++ Types/User.cs
@@ -0,0 +1,8 @@
+﻿namespace PrenburtisBot.Types
+{
+	internal class User(long userId, string firstName, double rating, Gender gender, bool canVote, bool needVote) : Player(userId, firstName, rating, gender)
+	{
+		public readonly bool CanVote = canVote;
+		public readonly bool NeedVote = needVote;
+	}
+}
