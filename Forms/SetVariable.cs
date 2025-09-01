64cd68e276edad75afcf46546999d51b66c5358e
Vasilii <pimenovasya@gmail.com>
2025-09-01 17:50:06 +0300

    Added class SetVariable

---
+++ Forms/SetVariable.cs
@@ -0,0 +1,32 @@
+﻿using PrenburtisBot.Attributes;
+using PrenburtisBot.Types;
+
+namespace PrenburtisBot.Forms
+{
+	[BotCommandChat("Задать переменную среды", "BOT_OWNER_CHAT_ID")]
+	internal class SetVariable : BotCommandFormBase
+	{
+		private string? _variable;
+
+		public static string Render() => "Введите имя переменной среды";
+		public string Render(string value)
+		{
+			if (_variable is null)
+			{
+				_variable = value;
+				return "Введите значение переменной среды";
+			}
+
+			string variable = _variable;
+			_variable = null;
+			return Render(variable, value);
+		}
+		public static string Render(string variable, string? value)
+		{
+			if (value == "null")
+				value = null;
+			Environment.SetEnvironmentVariable(variable, value);
+			return $"{variable} = {Environment.GetEnvironmentVariable(variable)}";
+		}
+	}
+}
