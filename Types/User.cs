65f5e44fb24595833fe97df55fff46b5c881d8e1
Vasilii <pimenovasya@gmail.com>
2025-12-26 13:15:05 +0300

    Removed properties CanVote and NeedVote

--- Types/User.cs
+++ Types/User.cs
@@ -1,10 +1,8 @@
 ﻿namespace PrenburtisBot.Types
 {
-	internal class User(long userId, string firstName, double rating, Gender gender, Skills skills, bool canVote, bool needVote, bool isArchived) 
+	internal class User(long userId, string firstName, double rating, Gender gender, Skills skills, bool isArchived) 
 		: Player(userId, firstName, rating, gender, skills)
 	{
-		public readonly bool CanVote = canVote;
-		public readonly bool NeedVote = needVote;
 		public readonly bool IsArchived = isArchived;
 	}
 }
