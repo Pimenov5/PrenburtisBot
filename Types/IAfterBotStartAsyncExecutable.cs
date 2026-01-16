775d16446548c0742139711fe25e77a0350d0909
Vasilii <pimenovasya@gmail.com>
2026-01-16 12:38:35 +0300

    Added interface IAfterBotStartAsyncExecutable

---
+++ Types/IAfterBotStartAsyncExecutable.cs
@@ -0,0 +1,10 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace PrenburtisBot.Types
+{
+	internal interface IAfterBotStartAsyncExecutable
+	{
+		public Task<TextMessage> ExecuteAsync(ITelegramBotClient botClient, ChatId chatId, int? messageThreadId, params string[] args);
+	}
+}
